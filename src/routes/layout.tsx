f19aab686754cfb6015feae4307f5a8688f1fbe9
Rado <radonirina.maminiaina@gmail.com>
2026-03-18 23:56:13 +0300

    add pages

---
+++ src/routes/layout.tsx
@@ -0,0 +1,23 @@
+import { component$, Slot } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+      </ul>
+      <div>
+        <Slot />
+      </div>
+    </>
+  );
+});
